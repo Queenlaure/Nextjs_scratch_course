39665870e30cd788b4291b7ec43b6d3042fcff97
Nfongeh Queen <queennfongeh@gmail.com>
2022-08-26 16:44:42 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const articles = [
+    {
+        id: '1',
+        title: 'Github introduces dark mode and auto-merge pull request',
+        excerpt: 'Github today announced a bunch of new features at its virtual Github...',
+        body: 'Github today announced a bunch of new features at its virtual Github Universe conference including dark mode, auto-merge pull requests, and Enterprise Server 3.0. In the past couple of years, almost all major apps have rolled out a dark theme for its users, so why not Github?'
+    },
+    {
+        id: '2',
+        title: 'Github introduces dark mode and auto-merge pull request',
+        excerpt: 'Github today announced a bunch of new features at its virtual Github...',
+        body: 'Github today announced a bunch of new features at its virtual Github Universe conference including dark mode, auto-merge pull requests, and Enterprise Server 3.0. In the past couple of years, almost all major apps have rolled out a dark theme for its users, so why not Github?'
+    },
+    {
+        id: '3',
+        title: 'Github introduces dark mode and auto-merge pull request',
+        excerpt: 'Github today announced a bunch of new features at its virtual Github...',
+        body: 'Github today announced a bunch of new features at its virtual Github Universe conference including dark mode, auto-merge pull requests, and Enterprise Server 3.0. In the past couple of years, almost all major apps have rolled out a dark theme for its users, so why not Github?'
+    },
+    {
+        id: '4',
+        title: 'Github introduces dark mode and auto-merge pull request',
+        excerpt: 'Github today announced a bunch of new features at its virtual Github...',
+        body: 'Github today announced a bunch of new features at its virtual Github Universe conference including dark mode, auto-merge pull requests, and Enterprise Server 3.0. In the past couple of years, almost all major apps have rolled out a dark theme for its users, so why not Github?'
+    }
+]
